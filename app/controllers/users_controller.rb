7238944ef153878373c8c3a9aa24cf56f41414c3
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-10-08 10:52:08 +0900

    usersコントローラ入力ミスの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
+  def create
+    @user = User.new(params_user)
+    if @user.save
+      redirect_to 
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def params_user
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
