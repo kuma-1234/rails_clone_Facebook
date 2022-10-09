56de90e4ffb51322c2bee8a71e48e70c5adae93c
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-10-09 20:39:29 +0900

    blogとuser：アソシエーションの実装

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,55 @@
 class BlogsController < ApplicationController
+
+  def index
+    @blogs = Blog.all
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(params_blog)
+    @blog.user = current_user
+    if params[:back]
+      render :new
+    else
+      if @blog.save
+        flash[notice] = "投稿の作成をしました！"
+        redirect_to new_blog_path
+      else
+        render :new
+      end
+    end
+  end
+
+  def confirm
+    @blog = blog.new(params_blog)
+    @blog.user = current_user
+    render :new if @blog.invalid?
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update(params_blog)
+      redirect_to blogs_path, notice: "ブログを編集しました。"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def params_blog
+    params.require(:blog).permit(:title, :content, :image, :image_cache )
+  end
+
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,5 @@
 class Blog < ApplicationRecord
+  belongs_to :user
   mount_uploader :image, ImageUploader
   validates :title, presence: true
   validates :content, presence: true, length: { maximum:60 }
