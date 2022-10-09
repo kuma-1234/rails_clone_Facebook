e8c69ad499c495405124838a1e1084d1be20a5ba
kuma-1234 <112454888+kuma-1234@users.noreply.github.com>
2022-10-09 17:42:18 +0900

    blogモデルのmigrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_07_125309) do
+ActiveRecord::Schema.define(version: 2022_10_09_082021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.text "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
